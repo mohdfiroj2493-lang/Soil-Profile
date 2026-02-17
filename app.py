00520b1c36167e1ec9cd523fb799f85ac03840ae
mohdfiroj2493-lang <mohdfiroj2493@gmail.com>
2026-02-16 19:05:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ SOIL_HATCH_MAP = {
     "Rock": "xx", "PWR": "xx", "RF": "++",
     "CLAYSTONE": "- - -", "SANDY CLAYSTONE": "///", "SILTY CLAYSTONE": "+++",
     "SILTSTONE": "....", "SANDY SILTSTONE": "///",
-    "CONGLOMERATE": "OOO",
+    "CONGLOMERATE": "O",
     "SANDSTONE": "....", "CLAYEY SANDSTONE": "/././.", "SILTY SANDSTONE": "+++", 
     "SHALE": "----",
 }
